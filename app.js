9239f8b163b5ac0c4c098fcf0f51e8960c09e120
shubham <shubhamagarwal0632@gmail.com>
2025-01-27 14:34:23 +0530

    first commit

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const app = express();
 // Define the port for your server
 const PORT = 3000;
 
+app.get('/',(req, res)=>{
+    res.send('server is running fine for the testing');
+})
+
 // Redirect endpoint
 app.get('/redirect', (req, res) => {
   res.redirect('https://example.com'); // Replace with your target URL
